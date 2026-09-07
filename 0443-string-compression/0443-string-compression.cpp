06a81f2f4e6b4c8f2d6fd47e1dab17873ed76493
Prathmesh dafare <dafareprathmesh41@gmail.com>
2026-09-07 21:44:42 +0530

    Time: 3 ms (12.8%), Space: 13.8 MB (50.25%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -13,16 +13,16 @@ public:
             }
 
             if(count == 1){
-                chars[idx++] = ch; // only the character store at the chars of idx 
-                      // if store on element at one index we should have to increase the index 
+                chars[idx] = ch; // only the character store at the chars of idx 
+                      idx++;// if store on element at one index we should have to increase the index 
             }
             else{ // if the count is not equal to the 1 then we have to store the character as well as the count in the chars 
-                chars[idx++] = ch; // it store the charecter at the index idx 
-                      // increase the index by one 
+                chars[idx] = ch; // it store the charecter at the index idx 
+                      idx ++;// increase the index by one 
                 string str = to_string(count); // as the count is in the form of the integer we need to conver that integer into the string hence the to_string(count); is used
                 for(char dig : str){ // now the count should be 2 digits or 3 digits we have to store every digit in the saperate index 
-                    chars[idx++] = dig;
-                
+                    chars[idx] = dig;
+                     idx++;
                 }
             }
             i--; // while comparing the index i reached to the extra i++ so we have to do the i--
